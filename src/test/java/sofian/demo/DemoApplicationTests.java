596f3a426bd6b0d4da6f8c707f229be352bceb66
HYCM0184 <bobycontactpro@gmail.com>
2025-06-29 14:59:04 +0200

    Initial Commit

---
+++ src/test/java/sofian/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package sofian.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
